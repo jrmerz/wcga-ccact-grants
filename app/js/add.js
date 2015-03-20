a2e0244993dec310d6021c6359dd254f2b2a5854
Justin Merz <jrmerz@gmail.com>
2015-03-20 19:37:54 -0400

    adding user grant removal, adding admin viewing of blacklist

--- app/js/add.js
+++ app/js/add.js
@@ -11,7 +11,8 @@ WCGA.add = (function() {
                     '<h3 class="panel-title">Login</h3>'+
                 '</div>'+
                 '<div class="panel-body" style="text-align: center">'+
-                    '<div><a class="btn btn-default" href="/auth/google">Login with Google</a></div>'+
+                    '<div style="margin-bottom: 15px"><a class="btn btn-default" href="/auth/google"><i class="fa fa-google"></i> Login with Google</a></div>'+
+                    //'<div><a class="btn btn-default" href="/auth/yahoo"><i class="fa fa-yahoo"></i> Login with Yahoo</a></div>'+
                     '<div class="help-text">To suggest a grant you must first login in a Google Account.</div>'+
                 '</div>'+
             '</div>'+
@@ -60,14 +61,32 @@ WCGA.add = (function() {
             var html = '<ul class="list-group">';
             for( var i = 0; i < resp.length; i++ ) {
                 html += 
-                    '<li class="list-group-item">'+
+                    '<li class="list-group-item" grant="'+resp[i]._id +'">'+
                         '<span class="pull-right label '+getLabelClass(resp[i].status)+'">'+resp[i].status+'</span>'+
                         resp[i].title+'<br />'+
                         '<a href="'+resp[i].link+'" target="_blank">'+resp[i].link+'</a>'+
+                        '<div style="text-align:right"><a class="btn btn-danger remove-suggest-btn" style="font-size: 16px" grant="' + 
+                            resp[i]._id +'"><i class="fa fa-trash"></i></a></div>'+
                     '</li>';
             }
             html += '</ul>';
             $('#my-suggestions-content').html(html);
+
+            $('#my-suggestions-content').find('.remove-suggest-btn').on('click', function(){
+                if( !confirm('Are you sure you want to remove this suggestion?') ) return;
+
+                var btn = $(this);
+                btn.addClass('disabled').html('<i class="fa fa-spinner fa-spin"></i>');
+                $.get('/rest/removeSuggestion?id='+btn.attr('grant'), function(resp){
+                    btn.removeClass('disabled').html('<i class="fa fa-trash"></i>');
+
+                    if( resp.error ) return alert('Error removing grant :(');
+
+                    alert('Success!');
+                    $('li[grant="'+btn.attr('grant')+'"]').remove();
+                });
+                
+            });
         });
     }
 
--- app/js/admin.js
+++ app/js/admin.js
@@ -14,7 +14,42 @@ WCGA.admin = (function(){
         $('#username').text(WCGA.user.displayName);
         $('#status').on('change', reload);
 
+        $('a[role="tab"]').on('click', function() {
+            $('a[role="tab"]').parent().removeClass('active');
+            $(this).parent().addClass('active')
+
+            $('.tab-panel').hide();
+            $('#'+$(this).attr('panel')).show();
+
+            if( $(this).attr('panel') == 'blacklisted' ) loadBlacklist();
+        });
+
         reload();
+    }
+
+    function loadBlacklist() {
+        var panel = $('#blacklisted');
+        panel.html('<i class="fa fa-spinner fa-spin"></i> Loading blacklist...');
+
+        $.get('/rest/getBlacklist', function(resp){
+            if( resp.error ) {
+                return panel.html('<div class="alert alert-danger">Error loading blacklist :(</div>');
+            }
+            if( resp.length == 0 ) {
+                return panel.html('<div class="alert alert-info">No blacklisted items</div>');
+            }
+
+            var html = '<ul class="list-group">';
+            for( var i = 0; i < resp.length; i++ ) {
+                html += '<li class="list-group-item">' +
+                            '<h4>'+resp[i].title+'</h4>' +
+                            '<div><b>Reason:</b> '+resp[i].reason+'</div>' +
+                            '<div>By '+resp[i].user+' on '+(new Date(resp[i].timestamp).toLocaleDateString())+'</div>' +
+                        '</li>';
+            }
+
+            panel.html(html+'</ul>');
+        });
     }
 
     function reload() {
@@ -39,7 +74,7 @@ WCGA.admin = (function(){
                 '<div class="panel '+getStatusClass(currentStatus)+'" style="margin: 10px 20px" item="'+item._id+'">' +
                     '<div class="panel-heading">'+
                         item.title+' '+
-                        (item.history.length > 0 ? '<span class="pull-right updated-by">By '+item.history[item.history.length-1].user+'</span>' : '')+
+                        (item.history.length > 0 ? '<span class="pull-right updated-by">&nbsp;By '+item.history[item.history.length-1].user+'</span>' : '')+
                         createSelector(item._id, currentStatus)+
                     '</div>'+
                     '<div class="panel-body">'+
